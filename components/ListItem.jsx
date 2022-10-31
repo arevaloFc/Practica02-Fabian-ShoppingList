95c56f2c7ca12ef8a450fe51c30b5cb722ad33c4
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2022-10-31 11:35:00 +0000

    Feature(ListItem): Se completo el componente ListItem creando switch para las imagenes y otros cosas mas

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -1,15 +1,28 @@
 import { StyleSheet, Text, View, Image, Pressable} from 'react-native';
 
-const ListItem = () => {
+const ListItem = ({productName}) => {
+
+    let cambioImagenTipo = ''
+
+    switch (productName.type) {
+        case "fruit":
+                cambioImagenTipo = require('../assets/splash.png')
+            break;
+        case "fish":
+                cambioImagenTipo = require('../assets/icon.png')
+            break;
+        default:
+            break;
+    }
 
     return (
-        <Pressable>
-            <View>
-                <View>
-                    <Image/>
-                    <View>
-                        <Text>Nombre</Text>  
-                        <Text>Tipo</Text>
+        <Pressable style={styles.listItem}>
+            <View  style={{backgroundColor: 'red',width:'100%', marginBottom: 10}} >
+                <View style={styles.productCard} >
+                    <Image style={styles.productImage} source={cambioImagenTipo} />
+                    <View style={styles.text}>
+                        <Text style={styles.productName}>{productName.quantity} x </Text> 
+                        <Text style={styles.productName}>{productName.name}</Text>
                     </View>
                 </View>
             </View>  
@@ -18,7 +31,10 @@ const ListItem = () => {
 }
 
 const styles = StyleSheet.create({
-
+    productImage: {
+        width: 100,
+        height: 100
+    },
 });
 
 export default ListItem
